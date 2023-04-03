864c04e151a6c8ac64bff2ef526f14ed3686fb36
sashgit <oleksandr.pys@gmail.com>
2023-04-03 18:32:54 +1000

    CoreCount and ServerCount issue

--- Programs/ds-sim/src/pre-compiled/MyClient.java
+++ Programs/ds-sim/src/pre-compiled/MyClient.java
@@ -3,95 +3,93 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class MyClient.java
-{
+public class MyClient {
 
   public static void main(String[] args) throws Exception {
 
+    // initialisation of variables for servers and job handling
     Boolean flag = true;
     String largestType = null;
     int largestCore = 0;
     int serverCount = 0;
     int largestServerType = 0;
     int numServersOfLargestType = 0;
-   
+
+    // connection to the server
     Socket s = new Socket("127.0.0.1", 50000);
-   
+
+    // setup the input and output streams
     BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     DataOutputStream output = writeBytes("HELO\n");
-       
-      out.println("HELO"); // Send HELO
-        String username = "user.name";
-        String authMessage = "AUTH " + username;
-        out.println(authMessage); // Send AUTH username
 
-        String response = in.readLine();
-        System.out.println("Server says: " + response);
-   
-    while(true){
-    // send REDY
-    out.println("REDY"); // Send REDY
-            response = in.readLine(); // Receive a message
-           
-           // recieve JOBN
-           // Send GETS
-           // use gets by requesting server information
-           out.println("GETS All");
-           response = in.readLine();
-           
-           String[] parts = response.split("\\s+");
-           int jobID = Integer.parseInt(parts[2]);
-           int estRunTime = Integer.parseInt(parts[3]);
-           int core = Integer.parseInt(parts[4]);
-           int mem = Integer.parseInt(parts[5]);
+    // send the HELO message and the auth message
+    out.println("HELO"); // Send HELO
+    String username = "user.name";
+    String authMessage = "AUTH " + username;
 
-    // for loop used to locate the server of the largest type
-    // compare (use the compare function to find largest server) the core count and if you find the largest type you assign   all jobs to the server of that type
-    for(int i = 0; i < nRecs; i++){
-    //  Send a GETS message, e.g., GETS All
+    // Send AUTH username
+    out.println(authMessage);
 
-    //  Receive DATA nRecs recSize // e.g., DATA 5 124
+    // read server response and store in variable
+    String response = in.readLine();
 
+    // start to allocate jobs to servers
+    while (true) {
+      // send REDY
+      out.println("REDY"); // Send REDY
+      response = in.readLine(); // Receive a message
 
-    // Receive each record
-    response = input.readLine();
+      out.println("GETS All");
+      response = in.readLine();
 
+      // create an array of string that is stored in parts
+      String[] Segement = response.split("\\s+");
+      int jobID = Integer.parseInt(Segement[2]);
+      int runTimeEst = Integer.parseInt(Segement[3]);
+      int Core = Integer.parseInt(Segement[4]);
+      int Memory = Integer.parseInt(Segement[5]);
 
-    String[] jobInfo = response.split(" ");
-    int jobId = Integer.parseInt(jobInfo[0]);
-    int arrivalTime = Integer.parseInt(jobInfo[1]);
-    int estRuntime = Integer.parseInt(jobInfo[2]);
-    int coreCount = Integer.parseInt(jobInfo[4]);
-    // int memSize = Integer.parseInt(jobInfo[5]);
-    // int diskSize = Integer.parseInt(jobInfo[6]);
-    out.println("SCHD " + jobID + " " + largestServerType + " 0");
+      // for loop used to locate the server of the largest type
+      // compare (use the compare function to find largest server) the core count and
+      // if you find the largest type you assign all jobs to the server of that type
+      for (int i = 0; i < nRecs; i++) {
+        // Send a GETS message, e.g., GETS All
 
+        // Receive DATA nRecs recSize // e.g., DATA 5 124
 
-    
+        // Receive each record
+        response = input.readLine();
 
+        // using the SCHD use the jobID and largest serverType
+        String[] jobInfo = response.split(" ");
+        int jobId = Integer.parseInt(jobInfo[0]);
+        int coreCount = Integer.parseInt(jobInfo[4]);
 
-    // Keep track of the largest server type and the number of servers of that type
+        // if the current coreCount on the server is larger
+        // then the previous core count then you make that
+        // on the largest serverType
+        if (coreCount > serverCount) {
 
-    // End For
+        }
+
+        out.println("SCHD " + jobID + " " + largestServerType + " 0");
+
+        // Keep track of the largest server type and the number of servers of that type
+
+        // End For
+      }
+
+      // Send OK
+      output.writeBytes("OK\n");
+
+      // after we have found the server with the largest type we can start allocating
+      // jobs to this server
+      out.println("SCHD " + jobID + " " + largestServerType + " 0");
+    }
+
+    if (response.equals("NONE")) {
+      break;
+    }
+
   }
-
-    // Send OK
-    output.writeBytes("OK\n");
-
-    // Receive
-
-
-
-
-    // If the message received at Step 10 is JOBN then
-    // Schedule a job // SCHD
-    
-  
-   
-   
-   
-    // after we have found the server with the largest type we can start allocating jobs to this server
-    out.println("SCHD " + jobID + " " + largestServerType + " 0");
-            }
-       }
 }
